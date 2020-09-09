1d50e7ed534b36dd1bd0f07156474f491de57859
EC2 Default User <ec2-user@ip-172-31-42-227.ec2.internal>
2020-09-09 05:44:31 +0000

    first1

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'bd8deda881602068e8e3fd0f39e8cb0047d7e19a772b6a7b642e5691db4c8d5a33289133162713cd53cfcb7f28fda366ba8b9a40f6fdfef6d4b0b5a72af77382'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  root 'post_images#index'
+  devise_for :users
+
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resources :favarite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+end
+
